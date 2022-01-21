6014a8089ec2cdc53b7af3b1e76ca9ec9497c5e8
Cristian Capa <83434415+cristiancapa@users.noreply.github.com>
2022-01-21 18:26:45 -0500

    imagen reproductor - responsive

--- script.js
+++ script.js
@@ -2,6 +2,8 @@ const body = document.querySelector("body");
 const navbar = document.querySelector(".navbar");
 const menuBtn = document.querySelector(".menu-btn");
 const cancelBtn = document.querySelector(".cancel-btn");
+
+
 menuBtn.onclick = () => {
     navbar.classList.add("show");
     menuBtn.classList.add("hide");
@@ -15,3 +17,21 @@ cancelBtn.onclick = () => {
 window.onscroll = () => {
     this.scrollY > 20 ? navbar.classList.add("sticky") : navbar.classList.remove("sticky");
 }
+
+const audio = document.getElementById("audio");
+const playPause = document.getElementById("play");
+/* audio */
+
+
+
+playPause.addEventListener("click", () => {
+    if(audio.paused || audio.ended){
+        playPause.querySelector(".pause-btn").classList.toggle("hide");
+        playPause.querySelector(".play-btn").classList.toggle("hide");
+        audio.play();
+    }else{
+        audio.pause();
+        playPause.querySelector(".pause-btn").classList.toogle("hide");
+        playPause.querySelector(".play-btn").classList.toogle("hide");
+    }
+})
